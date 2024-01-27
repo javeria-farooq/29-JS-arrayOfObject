589e0a5f5de2e4c8a15d2c6ce9f7390b415917bf
javeria-farooq <gettingeducation6@gmail.com>
2024-01-27 12:47:00 +0500

    29 JS Assignment

---
+++ app.js
@@ -0,0 +1,43 @@
+// let obj = {
+//     age : '20',
+//     rollNo: '133517',
+//     arr : ['javeria ', 'farooq ', 'intermediate']
+// }
+
+// document.write(obj)  // abh ye print nh hga qk obj hmesha ek ek krke prnt hta h or jb hmn pura
+// ek sth prnt krwna h tb hmn ispe loop chlana prta h or obj ka loop srf or srf forIn h qk
+// ye bna hi srf or srf obj k lye h. or jb hmn obj ko akele mn prnt krwna h tb hm oose . se get
+// krte hn. t phle hm ek ek ko individually get krke check krke dkhte hn then uspe loop
+// chlaenge
+
+// document.write(obj.age, '<br>')
+// document.write(obj.rollNo, '<br>')
+// document.write(obj.arr)
+
+// for (var i = 0; i < obj.arr.length; i++) {
+//     document.write(obj.arr[i])
+// }
+
+// let objValues = Object.values(obj)
+// document.write(objValues)
+
+// for (var key in obj) {
+    // document.write(obj[key], '<br>')
+// }
+
+// document.write(Object.values(obj.arr))
+
+// for(var key in obj) {
+//     document.write(key, ' : ', obj[key], '<br>')
+// }
+
+    // for(var key in obj) {
+    //     document.write(key, ' : ', obj[key], '<br>')
+    //    for (var i = 0; i < obj.arr.length; i++) {
+    //     document.write(obj.arr[i], '<br>')
+    // }
+    // }
+
+
+// ok done h lkn ab kia hwa h obj mn jo keys hn wo as a string prnt hwi hn islye isne array ko
+// bh as a string prnt krwya h jski wjah se commas wgher sb aagae hn 
